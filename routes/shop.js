59d781af6fb586885885f3508a5ffd02f5e0720e
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2021-05-27 22:04:55 +0700

    fetch (all +single) from mongodb

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,10 @@ const shopController = require('../controllers/shop');
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct)
-router.post('/cart', shopController.postCart);
-router.get('/cart', shopController.getCart);
-router.get('/orders', shopController.getOrders);
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
-router.post('/create-order', shopController.postOrder);
+// router.post('/cart', shopController.postCart);
+// router.get('/cart', shopController.getCart);
+// router.get('/orders', shopController.getOrders);
+// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+// router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
-// const mysql = require('mysql2');
-// const pool = mysql.createPool({
-//     host:'localhost',
-//     user:'root',
-//     database:'node-complete',
-//     password:''
-// });
-//
-// module.exports = pool.promise();
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const Sequelize = require('sequelize');
+let _db;
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Jonathan:zftubW2CTITX9ga5@cluster0.a2aqy.mongodb.net/shop?retryWrites=true&w=majority')
+        .then(client => {
+                console.log('Connected');
+                _db = client.db();
+                callback(client);
+            }
+        )
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+}
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw  'No Database Found';
+}
 
-const sequelize = new Sequelize('node-complete', 'root', '', {dialect: 'mysql', host: 'localhost'});
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
-module.exports = sequelize;
